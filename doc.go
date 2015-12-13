4a0e6f8af23c1074dbcb2f08c817e4828ea5900c
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2015-12-13 21:09:05 +0100

    argh, doc again

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// Package grun is a small library to run in the default glib main loop from any goroutine.
+// Package grun is a small library to run functions in the default glib main loop from any goroutine.
 package grun
